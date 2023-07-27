d36a44d7f8679988d8a5646b4192ec4183cb9698
Pranish Kedia <pranishk12@gmail.com>
2023-07-27 12:21:29 +0100

    feat: retrieves user location

--- AutoLocateWeather.py
+++ AutoLocateWeather.py
@@ -10,3 +10,12 @@ def get_ip_address():
         print("Error getting IP address:", e)
     return None
 
+def get_location_info(ip_address):
+    try:
+        response = requests.get(f"https://ipinfo.io/{ip_address}")
+        if response.status_code == 200:
+            data = response.json()
+            return data
+    except Exception as e:
+        print("Error getting location info:", e)
+    return None
