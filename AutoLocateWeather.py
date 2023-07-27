1561ea6ca0bb9a0b99ce760660dca5467b1a90ae
Pranish Kedia <pranishk12@gmail.com>
2023-07-27 12:24:02 +0100

    feat: added function to retrieve weather from API

--- AutoLocateWeather.py
+++ AutoLocateWeather.py
@@ -19,3 +19,14 @@ def get_location_info(ip_address):
     except Exception as e:
         print("Error getting location info:", e)
     return None
+
+def get_weather_info(latitude, longitude, api_key):
+    try:
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+    except Exception as e:
+        print("Error getting weather info:", e)
+    return None
